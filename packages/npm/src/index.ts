c2c1af77c1fe301e9e13eda26d1919d174aee13f
Henrik Hofmeister <henrik@newdawn.dk>
2023-11-14 17:14:45 +0100

    include export

--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -7,5 +7,6 @@ export * from "./types";
 export * from "./helpers";
 export * from "./utils";
 export * from "./validate-entity";
+export * from "./resolve-dependencies";
 export * as Schemas from "../schemas";
 export { validateSchema } from "./validate-schema";
